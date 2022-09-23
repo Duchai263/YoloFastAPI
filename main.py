81da8a45cb8c5abbba16929b2392084c147366bb
Duchai263 <95674277+Duchai263@users.noreply.github.com>
2022-09-23 22:08:36 +0700

    Update main.py
    change how it works

--- main.py
+++ main.py
@@ -2,27 +2,30 @@ from fastapi import FastAPI, File, UploadFile,Form
 from fastapi.responses import Response
 from pathlib import Path
 import torch
-from fastapi.responses import JSONResponse
-import shutil
+import io
+import PIL.Image as Image
 
 app = FastAPI()
-model_dir = Path(r'C:\Users\ADMIN\Downloads\Compressed\exp\weights\best.pt')
-model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_dir)  # local model
 
 label = []
 name = []
 save_dir = Path(r"D:\Work")
+model_dir = Path(r'C:\Users\ADMIN\Downloads\Compressed\exp\weights\best.pt')
+model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_dir)  # local model
+img = []
 
 @app.post("/files")
-async def UploadImage(file: UploadFile = File(...)):
-    name.append(file.filename)
-    with open(save_dir/name[-1],"wb") as img:
-        shutil.copyfileobj(file.file,img)
-    return file.filename
+async def UploadImage(image: UploadFile = File(...)):
+    name.append(image.filename)
+    """with open(save_dir/name[-1],"wb") as img:
+        shutil.copyfileobj(image.file,img)"""
+    img.append(await image.read())
+    return image.filename
 
 @app.get("/files")
-async def results():
-    img = save_dir/name[-1]
-    label.append(model(img))
-    json = label[-1].pandas().xyxy[0].to_json(orient="records")
-    return json
+async def detect():
+    image = Image.open(io.BytesIO(img[-1]))
+    result = model(image)
+    label.append(result)
+    response = label[-1].pandas().xyxy[0].to_json(orient="records")
+    return Response(response)
