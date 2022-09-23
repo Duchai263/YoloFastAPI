c11f5d64485a9bc3c07eb2d0e64bf7659f58b3be
Duchai263 <95674277+Duchai263@users.noreply.github.com>
2022-09-23 20:02:24 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, File, UploadFile,Form
+from fastapi.responses import Response
+from pathlib import Path
+import torch
+from fastapi.responses import JSONResponse
+import shutil
+
+app = FastAPI()
+
+label = []
+name = []
+save_dir = Path(r"D:\Work")
+
+@app.post("/files")
+async def UploadImage(file: UploadFile = File(...)):
+    name.append(file.filename)
+    with open(save_dir/name[-1],"wb") as img:
+        shutil.copyfileobj(file.file,img)
+    return file.filename
+
+@app.get("/files")
+async def results():
+    model_dir = Path(r'C:\Users\ADMIN\Downloads\Compressed\exp\weights\best.pt')
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_dir)  # local model
+    img = save_dir/name[-1]
+    label.append(model(img))
+    json = label[-1].pandas().xyxy[0].to_json(orient="records")
+    return json
