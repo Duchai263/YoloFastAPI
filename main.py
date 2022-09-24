c155095a932ead27716ef971fee477a345bd3ae9
Duchai263 <95674277+Duchai263@users.noreply.github.com>
2022-09-24 18:49:34 +0700

    Update main.py
    add convert to 4 points

--- main.py
+++ main.py
@@ -34,3 +34,46 @@ async def UploadMultipleImages(images: list[UploadFile]):
         result_text = result_text + str(each) + '\n'
 
     return result_text
+
+#convert to 4 points
+def toxy(result):
+    pt1 = [result[0],result[1]]
+    pt2 = [result[0],result[3]]
+    pt3 = [result[2],result[1]]
+    pt4 = [result[2],result[3]]
+    convert = [ pt1, pt2, pt3, pt4, result[4], result[5], result[6] ]
+    return convert
+
+def ConvertTo4Pts(results):
+    a = (results.pandas().xyxy[0]).to_numpy()
+    b = []
+    
+    for each in a:
+        b.append(toxy(each))
+    
+    df = pd.DataFrame(b)
+    head = ['pt1','pt2','pt3','pt4','confidence','class','name']
+    df = df.set_axis(head, axis= 'columns')
+    return df
+
+@app.post("/files/to4pts")
+async def UploadImage(image: UploadFile = File(...)):
+    image = Image.open(io.BytesIO(await image.read()))
+    result = model(image)
+    response = ConvertTo4Pts(result).to_json(orient="records")
+    return Response(response)
+
+@app.post("/MultipleFiles/to4pts", response_class=PlainTextResponse)
+async def UploadMultipleImages(images: list[UploadFile]):
+    result = []
+    response = []
+    for each in images:
+        image =  Image.open(io.BytesIO(await each.read()))
+        result.append(model(image))
+        response.append(ConvertTo4Pts(result[-1]).to_json(orient="records"))
+    
+    result_text =''
+    for each in response:
+        result_text = result_text + str(each) + '\n'
+
+    return result_text
